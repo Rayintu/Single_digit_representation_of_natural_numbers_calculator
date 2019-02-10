a63b566d642f8376e4066957b931c6fcbed54978
Ricky <rickybroers.rb@gmail.com>
2019-02-10 23:16:08 +0100

    Files are set up. Now the real challenge begins, figuring this shit out

---
+++ src/main/kotlin/sronnCalculator.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println(getSingleDigitRepresentation(814, 2))
+}
+
+fun getSingleDigitRepresentation(threeDigitNumber: Int, singleDigit: Int): String {
+    return "Placeholder"
+}
